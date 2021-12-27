ede9f9d3db56ffc8ab621562a797936e649646f9
Pyosangwoo <psw3619@naver.com>
2021-12-27 23:31:56 +0900

    수정

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -3,6 +3,7 @@ import { Flex } from "@chakra-ui/react";
 import FileUpload from "./components/FileUpload";
 import { useMutation } from "react-query";
 import axios from "axios";
+import styled from "styled-components";
 
 const App = () => {
   const { mutate, data } = useMutation((file) => {    // data는 post 요청 받고 나서 돌아오는 결과값 
@@ -15,18 +16,22 @@ const App = () => {
     });
   });
 
+
+
   const onFileAccepted = (file) => {
     // console.log(file);
     mutate(file);
   };
 
   return (
-    <Flex w="100" h="1000px" alignItems="center" justifyContent="center">
-      {data && <div><div dangerouslySetInnerHTML={{__html: data.data.data}} /></div>   // data가 있으면 그걸 보여준다 
-      }    
-      {!!!data && <FileUpload onFileAccepted={onFileAccepted} />   //data가 없으면 파일업로드 페이지 보여준다
-      }
-    </Flex>
+    
+      <Flex w="100" h="1000px" alignItems="center" justifyContent="center">
+        {data && <div><div dangerouslySetInnerHTML={{__html: data.data.data}} /></div>   // data가 있으면 그걸 보여준다 
+        }    
+        {!!!data && <FileUpload onFileAccepted={onFileAccepted} />   //data가 없으면 파일업로드 페이지 보여준다
+        }
+      </Flex>
+
   );
 };
 
